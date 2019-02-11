d52946650074db9f2139a3f6df4a603cdc7fe77e
Jonathan Tuzman <tuzmusic@gmail.com>
2019-02-10 21:31:37 -0500

    create connection in environment firle

--- config/environment.rb
+++ config/environment.rb
@@ -10,11 +10,25 @@ require 'date'
 DBNAME = "learnmanager"
 
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
-Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
+# Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
 
-DBRegistry[ENV["ACTIVE_RECORD_ENV"]].connect!
-DB = ActiveRecord::Base.connection
-
-if ENV["ACTIVE_RECORD_ENV"] == "test"
-  ActiveRecord::Migration.verbose = false
+def connect
+  @connection = ActiveRecord::Base.establish_connection(
+    adapter: "sqlite3",
+    database: "../db/learnmanager-development.db",
+  )
+  create_tables unless tables_exist?
 end
+
+def tables_exist?
+  ActiveRecord::Base.connection.tables.size > 0
+end
+
+def create_tables
+  puts "database is empty..."
+  puts "creating tables"
+  path_to_current_file = File.expand_path(File.dirname(__FILE__))
+  load("#{path_to_current_file}/../db/schema.rb")
+end
+
+connect
--- db/migrate/20190210190844_create_lessons.rb
+++
@@ -1,8 +0,0 @@
-class CreateLessons < ActiveRecord::Migration[5.2]
-  def change
-    create_table :lessons do |t|
-      t.string :title
-      t.belongs_to :unit, index: true
-    end
-  end
-end
